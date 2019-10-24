4be73a7136a964b6fbaef00620a1c0874df07bfc
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2019-10-24 17:02:57 +0200

    Added experiments for comparison

---
+++ howto/20191024_comparison_gs_results/20191024_comparison_gs_results.R
@@ -0,0 +1,19 @@
+#' ---
+#' title: Comparison of GS Results
+#' date:  2019-10-24
+#' ---
+#' 
+#' ## Disclaimer
+#' Experiments on how to compare results between GS-evaluation runs
+#' 
+#' 
+#' ## Terminology
+#' Which of the output files contains which results?
+#' 
+#' BayesC.cgrRes1        -- posterior man for intercept and SD
+#' BayesC.cgrResSamples1 -- residual samples?
+#' BayesC.ghatREL1       -- different estimates: gHat, dereg.ebv, ... 
+#' BayesC.ip             -- Input parameter file 
+#' BayesC.log            -- Log file
+#' BayesC.mrkRes1        -- Marker effect results for each animal
+#' BayesC.out1           -- Posterior Means of variance parameters at the end
