072db251ead89bf2d46c87e3a3a3b45ceb1df5de
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2020-01-20 16:56:44 +0100

    Changed title format.

--- regtests/sql/sql.sh
+++ regtests/sql/sql.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# SQL Commandline Client 
-#
-#
-#   Purpose:   Test Timezone Setting
-#   Author:    Peter von Rohr <peter.vonrohr@qualitasag.ch>
-#
-#######################################################################
-
+#' ---
+#' title: SQL Commandline Client 
+#' date:  2020-01-20 16:07:01
+#' author: Peter von Rohr
+#' ---
+#'
 set -o errexit    # exit immediately, if single command exits with non-zero status
 set -o nounset    # treat unset variables as errors
 set -o pipefail   # return value of pipeline is value of last command to exit with non-zero status
